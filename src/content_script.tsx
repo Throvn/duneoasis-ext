944e551b77ef134c932fcdd0d51915c066e03bbe
Louis <tagnacht2@gmail.com>
2022-09-18 03:46:17 +0200

    added ui and not yet working ui elements

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,3 +1,6 @@
+import { initializeBlockies } from './setupBlockly';
+import { getElementByXpath } from './helper';
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.color) {
     console.log("Receive color = " + msg.color);
@@ -7,3 +10,31 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     sendResponse("Color message is none.");
   }
 });
+
+
+onload = function () {
+  console.log("Hello from Dune Oasis.");
+  const loadingInterval = setInterval(() => {
+    const editorWindow: Node | null = getElementByXpath("/html/body/div/div/main/div/section/div/div[1]/section[1]");
+    if (editorWindow) {
+      clearInterval(loadingInterval);
+      const $editorWindow: HTMLElement = editorWindow as HTMLElement;
+      console.log($editorWindow)
+
+      const parentOfEditor = $editorWindow.parentElement;
+
+      const $blocklyDiv = document.createElement("div");
+      $blocklyDiv.id = "blocklyDiv";
+      $blocklyDiv.style.width = "100%";
+      console.log("editorVeiwHeights:" + $editorWindow.clientHeight);
+      $blocklyDiv.style.height = $editorWindow.clientHeight + "px";
+      $blocklyDiv.style.display = "block";
+      $blocklyDiv.style.border = "red solid 1px";
+      parentOfEditor?.insertAdjacentElement("afterbegin", $blocklyDiv);
+
+      $editorWindow.style.display = "none";
+
+      initializeBlockies($blocklyDiv);
+    }
+  }, 300)
+}
